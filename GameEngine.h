81badc802eaef902d3ac5e58c85238ef4d137f4f
liyh01 <l0y0h0l0@163.com>
2018-05-11 13:56:33 +0800

    init repository

---
+++ GameEngine.h
@@ -0,0 +1,11 @@
+#ifndef _GAME_ENGINE_H
+#define _GAME_ENGINE_H
+
+#include <windows.h>
+#include <string>
+#include <map>
+#include "GameInput.h"
+#include "GameOutput.h"
+
+
+#endif
---
+++ WinMain.cpp
@@ -0,0 +1,6 @@
+#include "GameEngine.h"
+
+int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+
+}
